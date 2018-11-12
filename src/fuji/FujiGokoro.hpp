d1372d881cf87f444f240b4bb25d686b133a2990
cllightz <cllightz@gmail.com>
2018-11-12 20:57:55 +0900

    remove debug output

--- src/fuji/FujiGokoro.hpp
+++ src/fuji/FujiGokoro.hpp
@@ -517,19 +517,19 @@ namespace UECda{
                 root.sort();
 
                 // DEBUG:
-#ifndef POLICY_ONLY
-                if(!fieldInfo.isMate() && !fieldInfo.isGiveUp()){
-                    auto& child = root.child;
-                    int candidates = root.candidates;
-                    if(candidates > 2){
-                        cout << "DEBUG TS: " << candidates << " candidates, " << root.allSimulations << " simulations." << endl;
-                        for(int c = 0; c < candidates; ++c){
-                            cout << c << ":\t" << child[c].mean() << '\t' << child[c].monteCarloScore.a << '\t' << child[c].monteCarloScore.b << '\t' << child[c].policyScore << '\t' << child[c].policyProb << /*'\t' << child[c].toString() <<*/ endl;
-                        }
-                        cout << endl;
-                    }
-                }
-#endif
+// #ifndef POLICY_ONLY
+//                 if(!fieldInfo.isMate() && !fieldInfo.isGiveUp()){
+//                     auto& child = root.child;
+//                     int candidates = root.candidates;
+//                     if(candidates > 2){
+//                         cout << "DEBUG TS: " << candidates << " candidates, " << root.allSimulations << " simulations." << endl;
+//                         for(int c = 0; c < candidates; ++c){
+//                             cout << c << ":\t" << child[c].mean() << '\t' << child[c].monteCarloScore.a << '\t' << child[c].monteCarloScore.b << '\t' << child[c].policyScore << '\t' << child[c].policyProb << /*'\t' << child[c].toString() <<*/ endl;
+//                         }
+//                         cout << endl;
+//                     }
+//                 }
+// #endif
                 
                 // 以下必勝を判定したときのみ
                 if(fieldInfo.isMate()){
