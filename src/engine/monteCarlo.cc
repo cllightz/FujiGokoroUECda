669de5d8fde2096cbc828d78c1b062b46c904f9c
cllightz <cllightz@gmail.com>
2019-09-16 21:40:35 +0900

    逐次削除方策の仮実装
    UCB<LCBではなく3σで枝刈り判定

--- src/engine/monteCarlo.cc
+++ src/engine/monteCarlo.cc
@@ -8,132 +8,7 @@ namespace Settings {
     constexpr double valuePerClock = 5.0 / (THINKING_LEVEL * THINKING_LEVEL) / std::pow(10.0, 10);
 
     // 時間の価値(1秒あたり),3191は以前のPCのクロック周波数(/microsec)なので意味は無い
-    constexpr double valuePerSec = valuePerClock * 3191 * std::pow(10.0, 6); 
-}
-
-// バンディット手法により次に試す行動を選ぶ
-inline int selectBanditAction(const RootInfo& root, Dice& dice, std::bitset<N_MAX_MOVES>& pruned, int& prunedCandidates) {
-    int actions = root.candidates;
-    const auto& a = root.child;
-    if (actions == 2) {
-        // 2つの時は同数(分布サイズ単位)に割り振る
-        if (a[0].size() == a[1].size()) return dice() % 2;
-        else return a[0].size() < a[1].size() ? 0 : 1;
-    } else {
-        // 枝刈り処理
-        constexpr int HYPER_PARAM_PRUNE_SIZE_THRE = 100;
-        constexpr int HYPER_PARAM_PRUNE_CAND_MIN = 5;
-        constexpr int HYPER_PARAM_PRUNE_SIMS_THRE = 50;
-        constexpr double HYPER_PARAM_PRUNE_MEAN_THRE = 0.05;
-        int index = 0;
-        double allSize = root.monteCarloAllScore.size();
-        if(allSize >= HYPER_PARAM_PRUNE_SIZE_THRE && actions - prunedCandidates > HYPER_PARAM_PRUNE_CAND_MIN){
-            // スコアが最低値の候補を検索
-            double worstScore = DBL_MAX;
-            int worstIndex = -1;
-            for(int c = 0; c < actions; ++c){
-                double tmpMean = a[c].mean();
-                if(!pruned[c]){
-                    if(a[c].simulations >= HYPER_PARAM_PRUNE_SIMS_THRE){
-                        if(tmpMean < HYPER_PARAM_PRUNE_MEAN_THRE){
-                            if(tmpMean < worstScore){
-                                worstScore = tmpMean;
-                                worstIndex = c;
-                            }
-                        }
-                    }
-                }
-            }
-
-            // 枝刈りが発生
-            if(worstIndex >= 0){
-                // std::cout << "DEBUG PRUNE!: " << a[worstIndex].mean() << '\t' << a[worstIndex].simulations << std::endl;
-                pruned[worstIndex] = true;
-                prunedCandidates++;
-            }
-        }
-
-        // 探索を進める候補を選ぶ
-        double bestScore = -DBL_MAX;
-        for(int c = 0; c < actions; ++c){
-            if(!pruned[c]){
-                // Thompson Sampling (報酬はベータ分布に従うと仮定)
-                // バンディットcの推定報酬値をベータ分布に従って乱数で定める
-                double tmpScore = a[c].monteCarloScore.rand(&dice);
-                if(tmpScore > bestScore){
-                    bestScore = tmpScore;
-                    index = c;
-                }
-            }
-        }
-
-        return index;
-    }
-}
-
-// Regretによる打ち切り判定
-inline bool finishCheck(const RootInfo& root, double simuTime, Dice& dice) {
-    // 腕の数
-    const int candidates = root.candidates;
-
-    const auto& child = root.child;
-    const double rewardScale = root.rewardGap;
-    constexpr int tryCount = 1600;
-    const double regretThreshold = tryCount * double(2 * simuTime * Settings::valuePerSec) / rewardScale;
-
-    struct Dist {
-        double mean; // 平均
-        double sem; // 標準偏差
-        double reg; // 損失
-    };
-
-    // 腕毎の損失のバッファ
-    std::array<Dist, N_MAX_MOVES> d;
-
-    // 腕毎の損失のバッファの初期化
-    for (int i = 0; i < candidates; i++) {
-        d[i] = {
-            child[i].mean(),
-            std::sqrt(child[i].mean_var()),
-            0
-        };
-    }
-
-    // TODO: tryCount回ループさせなくても損失は求まりそう
-    // TODO: ループさせるにしてもfor文の構造を工夫すればバッファの配列は不要そう
-    for (int t = 0; t < tryCount; t++) {
-        // 経験最高報酬
-        double bestValue = -100;
-
-        // 腕毎の評価値のバッファ
-        std::array<double, N_MAX_MOVES> value;
-
-        // 全腕に対して腕を引いてみる
-        for (int i = 0; i < candidates; i++) {
-            // 正規分布に従う乱数生成器
-            std::normal_distribution<double> nd(d[i].mean, d[i].sem);
-
-            // 腕を引いた結果
-            value[i] = nd(dice);
-
-            // 経験最高報酬の更新
-            bestValue = std::max(bestValue, value[i]);
-        }
-
-        // 全腕の損失を加算
-        for (int i = 0; i < candidates; i++) {
-            d[i].reg += bestValue - value[i];
-        }
-    }
-
-    // 損失が大きい場合は十分に最適腕が求まったとして終了させる
-    for (int i = 0; i < candidates; i++) {
-        if (d[i].reg < regretThreshold) {
-            return true;
-        }
-    }
-
-    return false;
+    constexpr double valuePerSec = valuePerClock * 3191 * std::pow(10, 6); 
 }
 
 void MonteCarloThread(const int threadId, const int numThreads,
@@ -155,13 +30,13 @@ void MonteCarloThread(const int threadId, const int numThreads,
     // プールした生成済みの世界
     std::array<World, 128> worlds;
 
-    // 世界生成のためのクラスを初期化
-
+    #pragma region 世界生成のためのデータを初期化
     // 今の試合のプレイ履歴
     const auto& record = pshared->record.latestGame();
 
     // 世界生成器(手札推定器)
     RandomDealer estimator(*pfield, myPlayerNum);
+    #pragma endregion 世界生成のためのデータを初期化
 
     // 現在の盤面情報の複製
     Field pf = *pfield;
@@ -178,6 +53,19 @@ void MonteCarloThread(const int threadId, const int numThreads,
     std::bitset<N_MAX_MOVES> pruned;
     int prunedCandidates = 0;
 
+    // 各腕に同じ世界を選択するためのカウンタ
+    int worldSelectingCounter = 0;
+
+    // 使用する世界の番号
+    int world = -1;
+
+    // 腕の削除の間隔を管理するカウンタ
+    int pruningCounter = 0;
+    constexpr int pruningInterval = 10;
+
+    // 選択する腕のインデックス
+    int action = -1;
+
     uint64_t simuTime = 0ULL; // プレイアウトと雑多な処理にかかった時間
     uint64_t estTime = 0ULL; // 局面推定にかかった時間
 
@@ -187,35 +75,85 @@ void MonteCarloThread(const int threadId, const int numThreads,
         clock.start();
     }
 
-    for (int i = 0; !proot->exitFlag; i++) { // 最大で最高回数までプレイアウトを繰り返す
-        // 使用する世界の番号
-        int world = 0;
-        int action = selectBanditAction(*proot, dice, pruned, prunedCandidates);          
+    for (int loop = 0; !proot->exitFlag; loop++) { // 最大で最高回数までプレイアウトを繰り返す
+        #pragma region 着手の選択
+        const auto& a = proot->child;
 
-        if (numSimulations[action] < numWorlds) {
-            // まだ全ての世界でこの着手を検討していない
-            world = numSimulations[action];
-        } else if (numThreads * numWorlds + threadId < (int)worlds.size()) {
-            // まだ十分な数だけ世界を生成していない場合、新しい世界を作成する
+        if (proot->candidates == 2) {
+            // 2つの時は同数(分布サイズ単位)に割り振る
+            action = loop % 2;
+        } else {
+            if (pruningCounter >= pruningInterval) {
+                pruningCounter = 0;
 
-            if (threadId == 0) {
-                simuTime += clock.restart();
-                worlds[numWorlds] = estimator.create(DealType::REJECTION, record, *pshared, ptools);
-                estTime += clock.restart();
-            } else {
-                worlds[numWorlds] = estimator.create(DealType::REJECTION, record, *pshared, ptools);
+                // 最も μ^-3σ^ が高い腕を検索
+                double bestScore = -DBL_MAX;
+                for (int c = 0; c < proot->candidates; c++) {
+                    if (!pruned[c]) {
+                        double tmpScore = a[c].mean() - 3 * std::sqrt(a[c].mean_var());
+                        if (tmpScore < bestScore) {
+                            bestScore = tmpScore;
+                        }
+                    }
+                }
+
+                // μ^+3σ^ < μ^*-3σ^* となる腕を枝刈り
+                for(int c = 0; c < proot->candidates; c++){
+                    if (!pruned[c]) {
+                        double tmpScore = a[c].mean() + 3 * std::sqrt(a[c].mean_var());
+                        if (tmpScore < bestScore) {
+                            // 枝刈りが発生
+                            if (threadId == 1) {
+                                std::cout << "DEBUG_PRUNE" << std::endl;
+                            }
+                            pruned[c] = true;
+                            prunedCandidates++;
+                        }
+                    }
+                }
             }
 
-            world = numWorlds++;
-        } else {
-            // ランダム選択
-            world = dice() % numWorlds;
+            // 次に探索する腕の決定
+            while (true) {
+                action++;
+                action %= proot->candidates;
+                if (!prune[action]) {
+                    break;
+                }
+            }
+        }
+        #pragma endregion 着手の選択
+        
+        #pragma region 世界の生成(手札推定)
+        if (worldSelectingCounter == 0) {
+            if (numThreads * numWorlds < (int)worlds.size()) {
+                // まだ十分な数だけ世界を生成していない場合、新しい世界を作成する
+
+                if (threadId == 0) {
+                    simuTime += clock.restart();
+                    worlds[numWorlds] = estimator.create(DealType::REJECTION, record, *pshared, ptools);
+                    estTime += clock.restart();
+                } else {
+                    worlds[numWorlds] = estimator.create(DealType::REJECTION, record, *pshared, ptools);
+                }
+
+                numWorlds++;
+            }
+
+            world++;
+            world %= numWorlds;
+            pruningCounter++;
         }
 
-        numSimulations[action]++;
-        numSimulationsSum++;
+        worldSelectingCounter++;
 
-        // シミュレーション実行
+        if (worldSelectingCounter >= proot->candidates - prunedCandidates) {
+            // 世界を固定して全腕に対して1回ずつ試行したら世界を生成する
+            worldSelectingCounter = 0;
+        }
+        #pragma endregion 世界の生成(手札推定)
+
+        #pragma region シミュレーション実行
         Field f;
         copyField(pf, &f);
         setWorld(worlds[world], &f);
@@ -226,20 +164,79 @@ void MonteCarloThread(const int threadId, const int numThreads,
         }
 
         proot->feedSimulationResult(action, f, pshared); // 結果をセット(排他制御は関数内で)
+        numSimulations[action]++;
+        numSimulationsSum++;
+        #pragma endregion シミュレーション実行
 
-        // 終了判定
+        #pragma region Regretによる打ち切り判定
+        // 終了判定はスレッド0でのみ判定する
         if (threadId == 0) {
             simuTime += clock.restart();
 
-            if (Settings::fixedSimulationCount < 0
-                && numSimulationsSum % std::max(4, 32 / numThreads) == 0
-                && proot->allSimulations > proot->candidates * 4) {
-                if (finishCheck(*proot, double(simuTime) / std::pow(10, 6), dice)) {
-                    proot->exitFlag = true;
-                    return;
+            if (Settings::fixedSimulationCount < 0 // プレイアウト回数が固定値で指定されていないこと
+                && numSimulationsSum % std::max(4, 32 / numThreads) == 0 // あまりにも頻繁に終了判定しないように
+                && proot->allSimulations > proot->candidates * 4) { // 腕の数の4倍以上プレイアウトしていること
+                auto uSecSimuTime = simuTime / std::pow(10, 6);
+                
+                const double rewardScale = proot->rewardGap;
+                constexpr int tryCount = 1600;
+                const double regretThreshold = tryCount * double(2 * uSecSimuTime * Settings::valuePerSec) / rewardScale;
+
+                struct Dist {
+                    double mean; // 平均
+                    double sem; // 標準偏差
+                    double reg; // 損失
+                };
+
+                // 腕毎の損失のバッファ
+                std::array<Dist, N_MAX_MOVES> d;
+
+                // 腕毎の損失のバッファの初期化
+                for (int i = 0; i < proot->candidates; i++) {
+                    d[i] = {
+                        proot->child[i].mean(),
+                        std::sqrt(proot->child[i].mean_var()),
+                        0
+                    };
+                }
+
+                // TODO: tryCount回ループさせなくても損失は求まりそう
+                // TODO: ループさせるにしてもfor文の構造を工夫すればバッファの配列は不要そう
+                for (int t = 0; t < tryCount; t++) {
+                    // 経験最高報酬
+                    double bestValue = -100;
+
+                    // 腕毎の評価値のバッファ
+                    std::array<double, N_MAX_MOVES> value;
+
+                    // 全腕に対して腕を引いてみる
+                    for (int i = 0; i < candidates; i++) {
+                        // 正規分布に従う乱数生成器
+                        std::normal_distribution<double> nd(d[i].mean, d[i].sem);
+
+                        // 腕を引いた結果
+                        value[i] = nd(dice);
+
+                        // 経験最高報酬の更新
+                        bestValue = std::max(bestValue, value[i]);
+                    }
+
+                    // 全腕の損失を加算
+                    for (int i = 0; i < candidates; i++) {
+                        d[i].reg += bestValue - value[i];
+                    }
+                }
+
+                // 損失が大きい場合は十分に最適腕が求まったとして終了させる
+                for (int i = 0; i < candidates; i++) {
+                    if (d[i].reg < regretThreshold) {
+                        proot->exitFlag = true;
+                        return;
+                    }
                 }
             }
         }
+        #pragma endregion Regretによる打ち切り判定
     }
 
     if (threadId == 1)
